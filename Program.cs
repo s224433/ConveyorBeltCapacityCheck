08be5a7ed1101717f0bfa5eeaa8d95b4eb6173dc
s224433 <aysenujin@gmail.com>
2025-09-14 21:11:46 +0200

    Færdig

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("How many motors are carrying the packages?");
+var motors = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("How many kg of packages do we expect?");
+var weight =double.Parse(Console.ReadLine());
+
+var avgLoadPerMotor = weight / motors;
+
+if (avgLoadPerMotor <= 5.6)
+{
+    Console.WriteLine("Yes! The conveyor belt can carry the packages.");
+}
+else
+{
+    Console.WriteLine("No! The conveyor belt can carry the packages."); 
+}
+//
